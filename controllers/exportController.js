32dc0027e7d5c590c0ab9076a2a7a721c5846c9d
Sk Saruk Ali <sksaruk.ali@teamgrid.io>
2025-06-03 18:45:29 +0530

    continue bug was solved

--- controllers/exportController.js
+++ controllers/exportController.js
@@ -1,12 +1,12 @@
- 
-const path          = require('path');
-const Course        = require('../models/Course');
+
+const path = require('path');
+const Course = require('../models/Course');
 const { buildXapiZip } = require('../utils/xapiZipBuilder');
 
 exports.exportCourse = async (req, res, next) => {
   try {
     const course = await Course.findOne({ _id: req.params.id, tenant: req.tenant })
-                               .populate('lessons topics assessments');
+      .populate('lessons topics assessments');
     if (!course) return res.sendStatus(404);
 
     const zipPath = await buildXapiZip(course);
